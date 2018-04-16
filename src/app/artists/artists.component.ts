a87e442be0e5e7a1656ac0a057de81cd74e7134a
galagarza89 <davegala89@gmail.com>
2018-04-16 18:38:26 -0400

    Update artists.component.ts

--- src/app/artists/artists.component.ts
+++ src/app/artists/artists.component.ts
@@ -12,6 +12,16 @@ export class ArtistsComponent implements OnInit {
   constructor(private http: Http) { }
 
     artists = [];
+    private _id: any;
+    private name: any;
+    private image_url: any;
+    private website_url: any;
+    private twitter_url: any;
+    private facebook_url: any;
+    private instagram_url: any;
+    private bio: any;
+    private info_url: any;
+  
     getData = function(){
     	this.http.get('./assets/artists.json').subscribe(
     		(res: Response) => {
